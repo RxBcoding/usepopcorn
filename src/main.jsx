cd05775a1abe7f9cf57146048d8d93ab86c655d9
Nathaniel McKain <63977243+RxBcoding@users.noreply.github.com>
2024-12-29 15:56:49 -0500

    create the star rating component and make it reusable

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import StarRating from "./StarRating.jsx";
+
+function Test() {
+  /* instead of lifting the rating state, we passed the setMovieRating function in as a prop and have the child component update
+  the movieRating for this test component whenever the internal star rating is updated */
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p> this movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </StrictMode>
 );
